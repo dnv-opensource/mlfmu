8ff0082bd277384d0b503533e0c279748a7ed761
Kristoffer Skare <kristoffer.skare@dnv.com>
2024-03-11 09:52:10 +0100

    Add variables and functions for state init in onnx fmu

--- templates/fmu/fmu_template.cpp
+++ templates/fmu/fmu_template.cpp
@@ -1,13 +1,16 @@
-#include "model_definitions.h"
 #include "fmu-uuid.h"
+#include "model_definitions.h"
+
 #include <cppfmu_cs.hpp>
 #include <onnxFmu.hpp>
 
 
-class {FmuName} : public OnnxFmu{{
-public:
-    {FmuName}(cppfmu::FMIString fmuResourceLocation) : OnnxFmu(fmuResourceLocation){{}}
-private:
+class
+{
+    FmuName
+} : public OnnxFmu {{
+    public :
+        {FmuName}(cppfmu::FMIString fmuResourceLocation) : OnnxFmu(fmuResourceLocation) {{}} private :
 }};
 
 
@@ -15,9 +18,13 @@ cppfmu::UniquePtr<cppfmu::SlaveInstance> CppfmuInstantiateSlave(
     cppfmu::FMIString /*instanceName*/, cppfmu::FMIString fmuGUID, cppfmu::FMIString fmuResourceLocation,
     cppfmu::FMIString /*mimeType*/, cppfmu::FMIReal /*timeout*/, cppfmu::FMIBoolean /*visible*/,
     cppfmu::FMIBoolean /*interactive*/, cppfmu::Memory memory, cppfmu::Logger /*logger*/)
-{{
-    if (std::strcmp(fmuGUID, FMU_UUID) != 0) {{
-        throw std::runtime_error("FMU GUID mismatch");
-    }}
-    return cppfmu::AllocateUnique<{FmuName}>(memory, fmuResourceLocation);
-}}
+{
+    {
+        if (std::strcmp(fmuGUID, FMU_UUID) != 0) {
+            {
+                throw std::runtime_error("FMU GUID mismatch");
+            }
+        }
+        return cppfmu::AllocateUnique<{FmuName}>(memory, fmuResourceLocation);
+    }
+}
