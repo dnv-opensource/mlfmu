c36733069fe912f65f85e6349fa5bde85d65a554
Claas <claas.rostock@dnv.com>
2024-10-16 14:11:47 +0200

    src/mlfmu/api.py : moved method `_find_default_file()` into a new module `pyth.py` in sub-package `utils`. IMplemented it there as a public function `find_default_file()`.  This change addresses https://github.com/dnv-innersource/mlfmu/pull/61#discussion_r1802907592

--- src/mlfmu/api.py
+++ src/mlfmu/api.py
@@ -7,6 +7,7 @@ from enum import Enum
 from pathlib import Path
 
 from mlfmu.utils import builder
+from mlfmu.utils.path import find_default_file
 
 __ALL__ = ["run", "MlFmuProcess"]
 
@@ -257,11 +258,11 @@ class MlFmuBuilder:
 
     def default_interface_file(self) -> Path | None:
         """Return the path to a interface json file inside self.root_directory if it can be inferred."""
-        return MlFmuBuilder._find_default_file(self.root_directory, "json", "interface")
+        return find_default_file(self.root_directory, "json", "interface")
 
     def default_model_file(self) -> Path | None:
         """Return the path to a ml model file inside self.root_directory if it can be inferred."""
-        return MlFmuBuilder._find_default_file(self.root_directory, "onnx", "model")
+        return find_default_file(self.root_directory, "onnx", "model")
 
     def default_build_folder(self) -> Path:
         """Return the path to a build folder inside the temp_folder. Creates the temp_folder if it is not set."""
@@ -312,66 +313,6 @@ class MlFmuBuilder:
     def default_fmu_output_folder(self) -> Path:
         """Return the path to the default fmu output folder."""
         return self.root_directory
-
-    # TODO @KristofferSkare: You could consider to move this method into a suitable module in utils,
-    #      and make it a public function therein. It could be useful for also other parts of the codebase.
-    #      CLAROS, 2024-10-15
-    @staticmethod
-    def _find_default_file(
-        folder: Path,
-        file_extension: str,
-        default_name: str | None = None,
-    ) -> Path | None:
-        """Return a file inside folder with the file extension that matches file_extension.
-
-        If there are multiple matches it uses the closest match to default_name if given.
-        Return None if there is no clear match.
-
-        Parameters
-        ----------
-        folder : Path
-            the folder to search in
-        file_extension : str
-            file extension to search for
-        default_name : str | None, optional
-            file name used to determine "closest match"
-            in case multiple files match file_extension, by default None
-
-        Returns
-        -------
-        Path | None
-            the path to the file if it is found, otherwise None
-        """
-        # Check if there is a file with correct file extension in current working directory. If it exists use it.
-        matching_files: list[Path] = []
-
-        for file in os.listdir(folder):
-            file_path = folder / file
-            if file_path.is_file() and file_path.suffix.lstrip(".") == file_extension:
-                matching_files.append(file_path)
-
-        if not matching_files:
-            return None
-
-        if len(matching_files) == 1:
-            return matching_files[0]
-
-        # If there are more matches on file extension. Use the one that matches the default name
-        if default_name is None:
-            return None
-
-        name_matches = [file for file in matching_files if default_name in file.stem]
-
-        if not name_matches:
-            return None
-
-        if len(name_matches) == 1:
-            return name_matches[0]
-
-        # If more multiple name matches use the exact match if it exists
-        name_exact_matches = [file for file in matching_files if default_name == file.stem]
-
-        return name_matches[0] if len(name_exact_matches) == 1 else None
 
 
 class MlFmuProcess:
